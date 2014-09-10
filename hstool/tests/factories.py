7cabeaa9c2be25aec6cc9aca60aa297ece8fa777
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-09-10 19:13:33 +0300

    [tests] write some user permissions tests

---
+++ hstool/tests/factories.py
@@ -0,0 +1,7 @@
+from factory.django import DjangoModelFactory
+
+from django.contrib.auth.models import User
+
+
+class UserFactory(DjangoModelFactory):
+    FACTORY_FOR = User
---
+++ hstool/tests/test_user_permissions.py
@@ -0,0 +1,66 @@
+from django.core.urlresolvers import reverse
+from django_webtest import WebTest
+
+from .factories import (
+    UserFactory,
+)
+
+
+class AuthenticatedListViewTests(WebTest):
+    def setUp(self):
+        self.user = UserFactory(username='username')
+
+    def test_assessments_authenticated(self):
+        url = reverse('home_view')
+        resp = self.app.get(url, user=self.user)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_drivers_authenticated(self):
+        url = reverse('drivers:list')
+        resp = self.app.get(url, user=self.user)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_indicators_authenticated(self):
+        url = reverse('indicators:list')
+        resp = self.app.get(url, user=self.user)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_sources_authenticated(self):
+        url = reverse('sources:list')
+        resp = self.app.get(url, user=self.user)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_figures_authenticated(self):
+        url = reverse('figures:list')
+        resp = self.app.get(url, user=self.user)
+        self.assertEqual(resp.status_code, 200)
+
+
+class AnonymousListViewTests(WebTest):
+    def setUp(self):
+        self.user = UserFactory(username='username')
+
+    def test_assessments_not_authenticated(self):
+        url = reverse('home_view')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_code, 403)
+
+    def test_drivers_not_authenticated(self):
+        url = reverse('drivers:list')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_code, 403)
+
+    def test_indicators_not_authenticated(self):
+        url = reverse('indicators:list')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_code, 403)
+
+    def test_sources_not_authenticated(self):
+        url = reverse('sources:list')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_code, 403)
+
+    def test_figures_not_authenticated(self):
+        url = reverse('figures:list')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_code, 403)
