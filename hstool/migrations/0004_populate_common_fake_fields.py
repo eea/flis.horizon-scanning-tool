7174af93d1a6937d56b6d52259cb52541767554f
Mihai Bivol <mm.bivol@gmail.com>
2014-11-14 16:38:39 +0200

    Change migrations to make use of common fixtures

--- hstool/migrations/0004_populate_common_fake_fields.py
+++ hstool/migrations/0004_populate_common_fake_fields.py
@@ -2,29 +2,45 @@
 from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import DataMigration
+from django.core.management import call_command
 from django.db import models
 
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        for country in orm.Country.objects.all():
-            new_c = orm['common.Country'](iso=country.iso, name=country.name)
-            new_c.save()
+        call_command('load_metadata_fixtures')
+        for generic_elem in orm.GenericElement.objects.all():
+            geo_scope = generic_elem.geographical_scope
+            geo_scope_title = geo_scope.title if geo_scope else None
+            model = orm['common.GeographicalScope']
+            try:
+                fake_geo_scope = model.objects.get(title=geo_scope_title)
+                generic_elem.fake_geographical_scope = fake_geo_scope
+            except model.DoesNotExist:
+                pass
 
-        for geo_scope in orm.GeographicalScope.objects.all():
-            new_g = orm['common.GeographicalScope'](
-                title=geo_scope.title,
-                require_country=geo_scope.require_country,
-                id=geo_scope.id)
-            new_g.save()
-        for env_theme in orm.EnvironmentalTheme.objects.all():
-            new_e = orm['common.EnvironmentalTheme'](
-                title=env_theme.title,
-                id=env_theme.id)
-            new_e.save()
+            country = generic_elem.country
+            country_name = country.name if country else None
+            model = orm['common.Country']
+            try:
+                fake_country = model.objects.get(name=country_name)
+                generic_elem.fake_country = fake_country
+            except model.DoesNotExist:
+                pass
+            generic_elem.save()
+
+        for indicator in orm.Indicator.objects.all():
+            env_theme = indicator.theme
+            theme_title = env_theme.title if env_theme else None
+            model = orm['common.EnvironmentalTheme']
+            try:
+                fake_theme = model.objects.get(title=theme_title)
+                indicator.fake_theme = fake_theme
+            except model.DoesNotExist:
+                pass
 
     def backwards(self, orm):
-        raise RuntimeError('This migration can not be undone')
+        raise RuntimeError("This migration can't be undone")
 
     models = {
         u'common.country': {
@@ -77,7 +93,7 @@ class Migration(DataMigration):
             'Meta': {'object_name': 'Figure'},
             'added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
             'author_id': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'file': ('hstool.models.ContentTypeRestrictedFileField', [], {'content_types': ['application/pdf', 'image/jpg', 'image/jpeg', 'image/png', 'image/gif']}),
+            'file': ('hstool.models.ContentTypeRestrictedFileField', [], {'content_types': ['application/pdf', 'image/jpg', 'image/jpeg']}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '512'})
         },
@@ -85,9 +101,11 @@ class Migration(DataMigration):
             'Meta': {'object_name': 'GenericElement'},
             'added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
             'author_id': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.Country']", 'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hstool.Country']", 'null': 'True', 'blank': 'True'}),
+            'fake_country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.Country']", 'null': 'True', 'blank': 'True'}),
+            'fake_geographical_scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.GeographicalScope']", 'null': 'True', 'blank': 'True'}),
             'figures': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['hstool.Figure']", 'null': 'True', 'blank': 'True'}),
-            'geographical_scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.GeographicalScope']", 'null': 'True', 'blank': 'True'}),
+            'geographical_scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hstool.GeographicalScope']", 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'short_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
@@ -102,8 +120,9 @@ class Migration(DataMigration):
         },
         u'hstool.indicator': {
             'Meta': {'object_name': 'Indicator', '_ormbases': [u'hstool.GenericElement']},
+            'fake_theme': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.EnvironmentalTheme']", 'null': 'True', 'blank': 'True'}),
             u'genericelement_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['hstool.GenericElement']", 'unique': 'True', 'primary_key': 'True'}),
-            'theme': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.EnvironmentalTheme']"}),
+            'theme': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hstool.EnvironmentalTheme']"}),
             'timeline': ('django.db.models.fields.IntegerField', [], {}),
             'year_base': ('django.db.models.fields.IntegerField', [], {}),
             'year_end': ('django.db.models.fields.IntegerField', [], {})
--- hstool/migrations/0005_auto__del_environmentaltheme__del_geographicalscope__del_country__del_.py
+++ hstool/migrations/0005_auto__del_environmentaltheme__del_geographicalscope__del_country__del_.py
@@ -17,29 +17,25 @@ class Migration(SchemaMigration):
         # Deleting model 'Country'
         db.delete_table(u'hstool_country')
 
+        # Deleting field 'GenericElement.country'
+        db.delete_column(u'hstool_genericelement', 'country_id')
+
+        # Deleting field 'GenericElement.geographical_scope'
+        db.delete_column(u'hstool_genericelement', 'geographical_scope_id')
+
+        # Deleting field 'Indicator.theme'
+        db.delete_column(u'hstool_indicator', 'theme_id')
+
+        # Remane fake fields
+        db.rename_column('hstool_indicator', 'fake_theme_id', 'theme_id')
+        db.rename_column('hstool_genericelement', 'fake_geographical_scope_id',
+                         'geographical_scope_id')
+        db.rename_column('hstool_genericelement', 'fake_country_id',
+                         'country_id')
+
 
     def backwards(self, orm):
-        # Adding model 'EnvironmentalTheme'
-        db.create_table(u'hstool_environmentaltheme', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
-        ))
-        db.send_create_signal(u'hstool', ['EnvironmentalTheme'])
-
-        # Adding model 'GeographicalScope'
-        db.create_table(u'hstool_geographicalscope', (
-            ('require_country', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
-        ))
-        db.send_create_signal(u'hstool', ['GeographicalScope'])
-
-        # Adding model 'Country'
-        db.create_table(u'hstool_country', (
-            ('iso', self.gf('django.db.models.fields.CharField')(max_length=2, primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
-        ))
-        db.send_create_signal(u'hstool', ['Country'])
+        raise RuntimeError("Cannot reverse this migration. 'Indicator.theme' and its values cannot be restored.")
 
 
     models = {
@@ -83,7 +79,7 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Figure'},
             'added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
             'author_id': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'file': ('hstool.models.ContentTypeRestrictedFileField', [], {'content_types': ['application/pdf', 'image/jpg', 'image/jpeg', 'image/png', 'image/gif']}),
+            'file': ('hstool.models.ContentTypeRestrictedFileField', [], {'content_types': ['application/pdf', 'image/jpg', 'image/jpeg']}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '512'})
         },
@@ -91,9 +87,9 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'GenericElement'},
             'added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
             'author_id': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.Country']", 'null': 'True', 'blank': 'True'}),
+            'fake_country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.Country']", 'null': 'True', 'blank': 'True'}),
+            'fake_geographical_scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.GeographicalScope']", 'null': 'True', 'blank': 'True'}),
             'figures': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['hstool.Figure']", 'null': 'True', 'blank': 'True'}),
-            'geographical_scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.GeographicalScope']", 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'short_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
@@ -102,8 +98,8 @@ class Migration(SchemaMigration):
         },
         u'hstool.indicator': {
             'Meta': {'object_name': 'Indicator', '_ormbases': [u'hstool.GenericElement']},
+            'fake_theme': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.EnvironmentalTheme']", 'null': 'True', 'blank': 'True'}),
             u'genericelement_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['hstool.GenericElement']", 'unique': 'True', 'primary_key': 'True'}),
-            'theme': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['common.EnvironmentalTheme']"}),
             'timeline': ('django.db.models.fields.IntegerField', [], {}),
             'year_base': ('django.db.models.fields.IntegerField', [], {}),
             'year_end': ('django.db.models.fields.IntegerField', [], {})
