1d010ef0c320f3727b6bf220fa1c2841d31f7399
Andreea Dima <andreeadima95@gmail.com>
2015-07-08 17:42:30 +0300

    [relation_update] Updated template. ref #25975

--- hstool/forms.py
+++ hstool/forms.py
@@ -112,10 +112,8 @@ class RelationForm(ModelForm):
             self.cleaned_data['destination'] = self.cleaned_data['driver']
         elif self.cleaned_data['figureindicator']:
             self.cleaned_data['destination'] = self.cleaned_data['figureindicator']
-            self.cleaned_data['description'] = ''
         elif self.cleaned_data['impact']:
             self.cleaned_data['destination'] = self.cleaned_data['impact']
-            self.cleaned_data['description'] = ''
         else:
             self._errors["destination"] = self.error_class([
                 'This field is required.'
