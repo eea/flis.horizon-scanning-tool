b9384cf985d00bc70cc68480b9c98296c1a01d74
Alex Eftimie <alex@eftimie.ro>
2015-10-14 14:30:03 +0200

    Hard squash migrations

--- hstool/migrations/0001_initial.py
+++ hstool/migrations/0001_initial.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import datetime
-import hstool.models
 import hstool.utils
+import hstool.models
 
 
 class Migration(migrations.Migration):
@@ -33,14 +33,11 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Figure',
+            name='DriverOfChangeType',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('draft', models.BooleanField(default=True)),
+                ('title', models.CharField(max_length=64)),
                 ('author_id', models.CharField(max_length=64)),
-                ('title', models.CharField(default=b'', max_length=512)),
-                ('file', hstool.models.ContentTypeRestrictedFileField(upload_to=hstool.utils.path_and_rename_figures)),
-                ('added', models.DateTimeField(default=datetime.datetime.now, auto_now_add=True)),
             ],
             options={
             },
@@ -53,7 +50,7 @@ class Migration(migrations.Migration):
                 ('draft', models.BooleanField(default=True)),
                 ('author_id', models.CharField(max_length=64)),
                 ('short_name', models.CharField(max_length=64)),
-                ('name', models.CharField(max_length=255)),
+                ('name', models.CharField(max_length=512)),
                 ('url', models.CharField(max_length=256, null=True, blank=True)),
                 ('added', models.DateTimeField(default=datetime.datetime.now, auto_now_add=True)),
             ],
@@ -62,79 +59,197 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='Figure',
+            fields=[
+                ('genericelement_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='hstool.GenericElement')),
+                ('file', hstool.models.ContentTypeRestrictedFileField(upload_to=hstool.utils.path_and_rename_figures)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('hstool.genericelement', models.Model),
+        ),
+        migrations.CreateModel(
             name='DriverOfChange',
             fields=[
                 ('genericelement_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='hstool.GenericElement')),
-                ('type', models.IntegerField(choices=[(1, b'Trends'), (2, b'Uncertainties'), (3, b'Wild Cards'), (4, b'Weak signals')])),
                 ('trend_type', models.IntegerField(default=1, choices=[(1, b'Trend'), (2, b'Megatrend')])),
                 ('uncertainty_type', models.IntegerField(default=1, choices=[(1, b'Rationale'), (2, b'Data'), (3, b'Methodology (related to the model)')])),
-                ('steep_category', models.CharField(max_length=5, choices=[(b'Ec', b'Economic'), (b'Env', b'Environmental'), (b'P', b'Political'), (b'S', b'Social'), (b'T', b'Technological')])),
-                ('time_horizon', models.IntegerField(choices=[(1, b'1 year'), (5, b'5 years'), (10, b'10 years'), (50, b'50 years'), (100, b'100 years'), (200, b'more than 100 years')])),
                 ('summary', models.TextField(null=True, blank=True)),
             ],
             options={
+                'abstract': False,
             },
-            bases=('hstool.genericelement',),
+            bases=('hstool.genericelement', models.Model),
+        ),
+        migrations.CreateModel(
+            name='Impact',
+            fields=[
+                ('genericelement_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='hstool.GenericElement')),
+                ('description', models.TextField()),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('hstool.genericelement', models.Model),
+        ),
+        migrations.CreateModel(
+            name='ImpactType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=64)),
+                ('author_id', models.CharField(max_length=64)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Implication',
             fields=[
                 ('genericelement_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='hstool.GenericElement')),
-                ('title', models.CharField(max_length=512)),
                 ('policy_area', models.CharField(default=0, max_length=64, null=True, blank=True, choices=[(b'mock_policy', b'Mock policy')])),
                 ('description', models.TextField(max_length=2048)),
             ],
             options={
+                'abstract': False,
             },
-            bases=('hstool.genericelement',),
+            bases=('hstool.genericelement', models.Model),
         ),
         migrations.CreateModel(
             name='Indicator',
             fields=[
                 ('genericelement_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='hstool.GenericElement')),
-                ('year_base', models.IntegerField()),
-                ('year_end', models.IntegerField()),
-                ('timeline', models.IntegerField(choices=[(5, b'5-year intermediate'), (0, b'continuous'), (1, b'daily'), (2, b'monthly'), (3, b'point'), (4, b'weekly'), (6, b'yearly')])),
-                ('theme', models.ForeignKey(to='common.EnvironmentalTheme')),
+                ('start_date', models.DateField(null=True, blank=True)),
+                ('end_date', models.DateField()),
+                ('assessment', models.TextField(null=True, blank=True)),
+                ('assessment_author', models.CharField(max_length=64, null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('hstool.genericelement', models.Model),
+        ),
+        migrations.CreateModel(
+            name='IndicatorFiles',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('file', models.FileField(null=True, upload_to=hstool.utils.path_and_rename_indicators, blank=True)),
+                ('indicator', models.ForeignKey(to='hstool.Indicator')),
             ],
             options={
             },
-            bases=('hstool.genericelement',),
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Relation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('draft', models.BooleanField(default=True)),
-                ('relationship_type', models.IntegerField(choices=[(1, b'Cause-effect relationship'), (2, b'Neutral relationship')])),
-                ('description', models.TextField(max_length=2048)),
+                ('relationship_type', models.IntegerField(blank=True, null=True, choices=[(1, b'Cause-effect relationship'), (2, b'Neutral relationship')])),
+                ('description', models.TextField(max_length=2048, null=True, blank=True)),
                 ('assessment', models.ForeignKey(related_name=b'relations', to='hstool.Assessment')),
-                ('destination', models.ForeignKey(related_name=b'dest_relations', to='hstool.GenericElement')),
-                ('figures', models.ManyToManyField(to='hstool.Figure', null=True, blank=True)),
-                ('source', models.ForeignKey(related_name=b'source_relations', to='hstool.GenericElement')),
             ],
             options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Source',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('draft', models.BooleanField(default=True)),
-                ('author_id', models.CharField(max_length=64)),
-                ('title', models.CharField(max_length=512)),
-                ('title_original', models.CharField(max_length=512)),
-                ('published_year', models.IntegerField()),
+                ('genericelement_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='hstool.GenericElement')),
+                ('title_original', models.CharField(max_length=512, null=True, blank=True)),
+                ('published_year', models.CharField(max_length=4)),
                 ('author', models.CharField(max_length=512)),
-                ('url', models.CharField(max_length=256)),
-                ('file', models.FileField(upload_to=hstool.utils.path_and_rename_sources)),
-                ('summary', models.TextField(max_length=2048)),
-                ('added', models.DateTimeField(default=datetime.datetime.now, auto_now_add=True)),
+                ('file', models.FileField(null=True, upload_to=hstool.utils.path_and_rename_sources, blank=True)),
+                ('summary', models.TextField(max_length=2048, null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=('hstool.genericelement',),
+        ),
+        migrations.CreateModel(
+            name='SteepCategory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=64)),
+                ('author_id', models.CharField(max_length=64)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TimeHorizon',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=64)),
+                ('author_id', models.CharField(max_length=64)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='relation',
+            name='destination',
+            field=models.ForeignKey(related_name=b'dest_relations', blank=True, to='hstool.GenericElement'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='relation',
+            name='figures',
+            field=models.ManyToManyField(to='hstool.Figure', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='relation',
+            name='indicators',
+            field=models.ManyToManyField(to='hstool.Indicator', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='relation',
+            name='source',
+            field=models.ForeignKey(related_name=b'source_relations', to='hstool.DriverOfChange'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='indicator',
+            name='sources',
+            field=models.ManyToManyField(to='hstool.Source', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='indicator',
+            name='theme',
+            field=models.ForeignKey(to='common.EnvironmentalTheme'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='implication',
+            name='sources',
+            field=models.ManyToManyField(to='hstool.Source', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='impact',
+            name='impact_type',
+            field=models.ForeignKey(related_name=b'impact_type', blank=True, to='hstool.ImpactType', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='impact',
+            name='sources',
+            field=models.ManyToManyField(to='hstool.Source', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='impact',
+            name='steep_category',
+            field=models.ForeignKey(related_name=b'impact_category', blank=True, to='hstool.SteepCategory', null=True),
+            preserve_default=True,
         ),
         migrations.AddField(
             model_name='genericelement',
@@ -144,20 +259,68 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='genericelement',
-            name='figures',
-            field=models.ManyToManyField(to='hstool.Figure', null=True, blank=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='genericelement',
             name='geographical_scope',
             field=models.ForeignKey(blank=True, to='common.GeographicalScope', null=True),
             preserve_default=True,
         ),
         migrations.AddField(
-            model_name='genericelement',
+            model_name='figure',
             name='sources',
             field=models.ManyToManyField(to='hstool.Source', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='figure',
+            name='theme',
+            field=models.ForeignKey(to='common.EnvironmentalTheme'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='figures',
+            field=models.ManyToManyField(to='hstool.Figure', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='impacts',
+            field=models.ManyToManyField(to='hstool.Impact', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='implications',
+            field=models.ManyToManyField(to='hstool.Implication', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='indicators',
+            field=models.ManyToManyField(to='hstool.Indicator', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='sources',
+            field=models.ManyToManyField(to='hstool.Source', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='steep_category',
+            field=models.ForeignKey(related_name=b'driver_category', to='hstool.SteepCategory'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='time_horizon',
+            field=models.ForeignKey(related_name=b'driver_time', to='hstool.TimeHorizon'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='driverofchange',
+            name='type',
+            field=models.ForeignKey(related_name=b'doc_type', to='hstool.DriverOfChangeType'),
             preserve_default=True,
         ),
     ]
